b974e9e89a685f5e3373fa9e6f67d45bb3d3cad7
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-09-04 22:53:53 +0200

    Git blame & lsp c++

--- lua/amelia/packer.lua
+++ lua/amelia/packer.lua
@@ -4,23 +4,28 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-	  vim.cmd("colorscheme rose-pine")
-  end})
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('ThePrimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('lvimuser/lsp-inlayhints.nvim')
-  use('m4xshen/autoclose.nvim')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('lvimuser/lsp-inlayhints.nvim')
+    use('m4xshen/autoclose.nvim')
+    use('f-person/git-blame.nvim')
 end)
