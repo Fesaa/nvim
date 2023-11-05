5cfee0d2ab97deb9ceafc67747fceea041ae22fa
Amelia <77553571+Fesaa@users.noreply.github.com>
2023-11-05 22:55:25 +0100

    Copilot keybind change

--- lua/amelia/remap.lua
+++ lua/amelia/remap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.api.nvim_set_keymap("i", "<Tab>", "copilot#Accept('<CR>')", {expr=true, silent=true})
-
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
